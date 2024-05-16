b3a91cb0cff2051f747020fc835b0bd88d014182
Bogdan Nikitin <nikitin.bogdan333@yandex.ru>
2024-05-16 12:23:36 +0300

    Definitions

---
+++ computer_club.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <cstdint>
+#include <unordered_map>
+#include <sstream>
+#include <string_view>
+
+
+constexpr std::int16_t FREE_TABLE = -1;
+
+class computer_club {
+private:
+    std::queue<std::string_view> pending_clients;
+    std::unordered_map<std::string, std::pair<std::size_t, std::int16_t>> tables;
+
+    std::stringstream& output;
+
+    std::uint16_t open_time;
+    std::uint16_t close_time;
+
+    std::size_t hour_cost;
+
+    void client_left_outgoing_event(std::int16_t time, std::string_view name);
+    void client_sat_outgoing_event(std::int16_t time, std::string_view name, std::size_t table_num);
+    void error(std::int16_t time, const std::string& message);
+public:
+    void client_came(std::int16_t time, const std::string& client_name);
+    void client_sat(std::uint16_t time, const std::string& client_name, std::size_t table_num);
+    void client_waiting(std::uint16_t time, const std::string& client_name);
+    void client_left(std::uint16_t time, const std::string& client_name);
+};
